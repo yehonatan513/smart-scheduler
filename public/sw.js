bd8aec3b888f5c7225138c9df9e5c5f493778140
yehonatan <yehonatanrec@gmail.com>
2026-03-05 02:02:12 +0200

    refresh cache setting

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,21 @@
-self.addEventListener('install', e => e.waitUntil(
-  caches.open('v1').then(cache => cache.addAll(['/']))
-))
+self.addEventListener('install', e => {
+  self.skipWaiting()
+})
 
-self.addEventListener('fetch', e => e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
-))
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    )
+  )
+})
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(fetch(e.request))
+    return
+  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  )
+})
